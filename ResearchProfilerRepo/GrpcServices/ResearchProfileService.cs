a7f15f8f12a48fb46b36cea5c1be39d3dfd71668
aldricaj <aldricaj@mail.uc.edu>
2020-01-17 16:22:03 -0500

    init commit

---
+++ ResearchProfilerRepo/GrpcServices/ResearchProfileService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace ResearchProfilerRepo
+{
+    public class ResearchProfileService : ResearcherService.ResearcherServiceBase
+    {
+        private readonly ILogger _logger;
+
+        public ResearchProfileService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ResearchProfileService>();
+        }
+
+        public override Task<ResearcherListReply> GetAllResearchers(GetAllResearcherRequest request, 
+            ServerCallContext context)
+        {
+            return null;
+        }
+
+        public override Task<ResearcherReply> GetResearcherById(ResearcherByIdRequest request,
+            ServerCallContext context)
+        {
+            return null;
+        }
+    }
+}
---
+++ ResearchProfilerTesting/IntegrationTests/ResearchProfileIntegrationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GrpcIntegrationTestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ResearchProfilerRepo;
+namespace ResearchProfiler.Tests.Integration 
+{
+    [TestClass]
+    public class ResearchProfileIntegrationTests : FunctionalTestBase<ResearchProfilerRepo.Startup>
+    {
+        [TestMethod]
+        public void TestSetup()
+        {
+            // Arrange
+            var client = new Greeter.GreeterClient(Channel);
+
+            // Act
+            var response = client.SayHello(new HelloRequest { Name = "Joe" });
+
+            // Assert
+            Assert.AreEqual("Hello Joe", response.Message);
+        }
+    }
+}
